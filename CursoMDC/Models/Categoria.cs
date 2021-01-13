fcd7f7ce76b8fe3fd9454287814a62b2567e71ea
unknown <andressasantosleao_cia@hotmail.com>
2021-01-13 11:20:49 -0300

    first commit

---
+++ CursoMDC/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoMDC.Models
+{
+    public class Categoria
+    {
+        public int id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo de descricao é Obrigatório")]
+        public string Descricao { get; set; }
+    }
+}
---
+++ CursoMDC/Models/ErrorMessageAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CursoMDC.Models
+{
+    internal class ErrorMessageAttribute : Attribute
+    {
+    }
+}
